c73e1d96b1243206b245e952eda0b43e1172e3fe
schecterchris@gmail.com <schecterchris@gmail.com>
2019-03-13 15:48:18 -0700

    Added comments for the sake of looking sane.

--- soaktimecalcconsole/Program.cs
+++ soaktimecalcconsole/Program.cs
@@ -6,16 +6,15 @@ namespace soaktimecalcconsole
     {
         static void Main(string[] args)
         {
-
+            //Variables
             DateTime currenttime = DateTime.Now;
-
             string atheathours;
             string atheatmins;
-
             string soak;
             string toleranceover;
             string toleranceunder;
 
+            //Get user input
             Console.WriteLine("Enter at heat date ("+currenttime.ToString("MM/dd/yyyy")+" for example)");
             DateTime inputtedDate = DateTime.Parse(Console.ReadLine());
 
@@ -34,25 +33,30 @@ namespace soaktimecalcconsole
             Console.WriteLine("Enter time tolerance underrun:");
             toleranceunder = Console.ReadLine();
 
-            
+            //Converted variables for new datetime object created with the at heat time in mind.
             int hours = Convert.ToInt32(atheathours);
             int minutes = Convert.ToInt32(atheatmins);
             int seconds = 00;
             var atheatobj = new DateTime(inputtedDate.Year, inputtedDate.Month, inputtedDate.Day, hours, minutes, seconds);
           
-
+            //Get a timespan object representing the difference between the current time and at heat time.
             TimeSpan difference = currenttime - atheatobj;
 
+            //Write total at heat time in minutes
             Console.WriteLine(difference.TotalMinutes.ToString() + " minutes have elapsed at heat.");
 
+            //Subtract soak time from total time to get the amount of minutes needed to soak
             int timerval = Convert.ToInt32(soak) - Convert.ToInt32(difference.TotalMinutes);
 
+            //High and low timer values utilizing the time tolerance
             int timerhigh = timerval + Convert.ToInt32(toleranceover) - 2;
             int timerlow = timerval - Convert.ToInt32(toleranceunder) + 2;
 
+            //Print what time the timer should be set to
             Console.WriteLine("Set timer between "+timerlow.ToString()+" and "+timerhigh.ToString()+" minutes.");
 
             Console.ReadLine();
+
         }
     }
 }
